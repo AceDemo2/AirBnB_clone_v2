20df1201a8e4bd3fd936fc2e71fb79e3a80a629e
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-17 19:26:48 -0700

    mysql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXITS hbnb_dev_db;
+CREATE USER IF NOT EXITS 'hbnb_dev_db'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hhbnb_dev_db.* TO 'hbnb_dev_db'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
