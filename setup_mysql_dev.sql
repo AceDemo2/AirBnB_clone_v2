2e9245e921be145c2c1af2eaeebe51c42037ff59
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-17 19:32:35 -0700

    mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+--create database and grant privileges
 CREATE DATABASE IF NOT EXITS hbnb_dev_db;
 CREATE USER IF NOT EXITS 'hbnb_dev_db'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hhbnb_dev_db.* TO 'hbnb_dev_db'@'localhost';
