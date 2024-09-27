c238c0bd9fccfc13d4d1f2b67c5d5b575777af87
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-27 07:53:37 -0700

    file storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""DBStorage"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+import os
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """initialization"""
+        user = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        database = os.getenv('HBNB_MYSQL_DB')
+        
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{password}@{host}/{database}')
+        if os.getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+    
+    def all(self, cls=None):
+        """query database"""
+        obj = {}
+        classes = [User, City, State, Review, Amenity, Place]
+        if not cls:
+            for i in classes:
+                query = self.__session.query(i).all()
+                for j in query:
+                    obj[f'{j.__class__.__name__}.{j.id}'] = j
+        else:
+            query = self.__session.query(cls).all()
+            for j in query:
+                obj[f'{i.__class__.__name__].{i.id}'] = query
+        return obj
+
+    def new(self, obj):
+        """new instance"""
+        self.__session.add(obj)
+
+    def save(self):
+        """save instance"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete instance"""
+        if not obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """reload database"""
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
+        self.__session = Session()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = (String(128), nullable=False)
+    
+    cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+
+    @property
+    def cities(self):
+        from models import storage
+        from models.city import City
+        """FileStorage relationship between State and City"""
+        city =[]
+        all_cities = storage.all(City)
+        for i in all_cities.values():
+            if self.id == city.state_id:
+                city.append(i)
+        return city
