daa5ee4c3adeaf0ec5d9e4f79bb8189a9b013458
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-27 07:59:39 -0700

    file storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
 import uuid
 from datetime import datetime
 
