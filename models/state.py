9538ab767f565158f36f37a190c131bf21adc91c
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-27 19:16:59 -0700

    sqlTest

--- models/state.py
+++ models/state.py
@@ -9,17 +9,18 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    
-    cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
-
-    @property
-    def cities(self):
-        from models import storage
-        from models.city import City
-        """FileStorage relationship between State and City"""
-        city =[]
-        all_cities = storage.all(City)
-        for i in all_cities.values():
-            if self.id == i.state_id:
-                city.append(i)
-        return city
+   
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            from models.city import City
+            """FileStorage relationship between State and City"""
+            city =[]
+            all_cities = storage.all(City)
+            for i in all_cities.values():
+                if self.id == i.state_id:
+                    city.append(i)
+            return city
