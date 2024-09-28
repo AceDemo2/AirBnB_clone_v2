84a4cb31c66b4b7269ab4f7e0d120681111efdb6
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-27 19:11:24 -0700

    sqlTest

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 type_store = getenv('HBNB_TYPE_STORAGE')
 if type_store == 'db':
